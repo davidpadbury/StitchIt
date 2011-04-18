de6587b25da3f180471ca17982efed79f1dd15e8
David Padbury <david@davidpadbury.com>
2011-04-17 22:11:41 -0400

    fixed multiline template bug

--- StitchIt/Handlers/HtmlTemplateFileHandler.cs
+++ StitchIt/Handlers/HtmlTemplateFileHandler.cs
@@ -14,7 +14,7 @@ namespace StitchIt.Handlers
         public string Build(string content)
         {
             // We're enclosing the javascript in a string so escape any single quote string delimiters
-            var escapedContent = content.Replace(@"'", @"\'");
+            var escapedContent = content.Replace(@"'", @"\'").Replace(Environment.NewLine, @"\n");
             var moduleContent = string.Format(TemplateModule, escapedContent);
 
             return moduleContent;
